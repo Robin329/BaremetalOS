ff400d4cd3009a874b567213e5d80253b192af23
robin <jiangrenbin329@gmail.com>
2023-08-17 02:45:27 +0000

    update flash tools
    Signed-off-by: robin <jiangrenbin329@gmail.com>

--- flash.sh
+++ flash.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-./rkbin/tools/loaderimage --pack --uboot u-boot-dtb.bin uboot.img
+./tools/loaderimage --pack --uboot u-boot-dtb.bin uboot.img
 sudo rkdeveloptool db rkbin/bin/rk33/rk3399_loader_v1.12.112.bin
 sleep 2
 sudo rkdeveloptool wl 0x4000 uboot.img
